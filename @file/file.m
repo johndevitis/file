cac19ff61b27a61e35286895cae0ddfc643e0602
John DeVitis <johndevitis@gmail.com>
2016-08-04 03:16:39 -0400

    refactored chk_name, added set method for name

--- @file/file.m
+++ @file/file.m
@@ -17,6 +17,7 @@ classdef file < matlab.mixin.SetGet
     
 %% dependent properties
 	properties (Dependent)
+        
         fullname % full file path/name.ext generated 
     end
 
@@ -34,44 +35,61 @@ classdef file < matlab.mixin.SetGet
             end
         end
         
-    %% ordinary
-        function chk_name(self,disp_chk)
-        %% chk_name
-        % * checks for an empty object name, throws an error if found. 
-        % * also checks for hidden path or extension info in name
-        % * use disp_chk = 1 to display current file path to user, else
-        % function only error screens null name
-        % * at end of fcn chk_name checks for missing ext field. this is so
-        % the nested filename.ext that is checked earlier can be sorted out
-        % before an error is thrown. and yes, it throws an error when
-        % missing.
-            if nargin < 2
-                disp_chk = 0; 
-            end;
-            % check for empty name
-            if isempty(self.name)
-                error('Name that thang!')
-            end
-            % check name contents
-            [pth,nm,xt]=fileparts(self.name);        % get name contents
-            if ~isempty(pth)                         % check for hidden path
-                self.path = fullfile(self.path,pth); % concat next pth
-                self.name = nm;                      % assign new name
-            end
-            % check for extension in name
-            if ~isempty(xt)     
-                self.ext = xt;  % assign next extension
-                self.name = nm; % assign new name
-            end
-            % check for empty extension
-            if isempty(self.ext)
-                error('Give an extension, mang!');
-            end
-            % current full name display check
-            if disp_chk == 1
-                fprintf('Current File: %s \n',self.fullname);
-            end
-        end
+%     %% ordinary
+%         function chk_name(self,disp_chk)
+%         %% chk_name
+%         % * checks for an empty object name, throws an error if found. 
+%         % * also checks for hidden path or extension info in name
+%         % * use disp_chk = 1 to display current file path to user, else
+%         % function only error screens null name
+%         % * at end of fcn chk_name checks for missing ext field. this is so
+%         % the nested filename.ext that is checked earlier can be sorted out
+%         % before an error is thrown. and yes, it throws an error when
+%         % missing.
+%             if nargin < 2
+%                 disp_chk = 0; 
+%             end;
+%             
+%             % check for empty name
+%             if isempty(self.name)
+%                 error('Name that thang!')
+%             end
+%             
+%                       
+%             % check name contents
+%             [pth,nm,xt]=fileparts(self.name);        % get name contents
+%             
+%             % -- path --
+%             if ~isempty(pth)                         % check for hidden path
+%                 self.path = fullfile(self.path,pth); % concat next pth
+%                 self.name = nm;                      % assign new name
+%             end
+%             
+%             % -- ext -- 
+%             % check for extension in name
+%             if ~isempty(xt)     
+%                 self.ext = xt;  % assign next extension
+%                 self.name = nm; % assign new name
+%             end
+%             
+%             % check for empty extension
+%             if isempty(self.ext)
+%                 error('Give an extension, mang!');
+%             end
+%             
+%             % chk file extension for missing dot
+%             if ~strcmp(self.ext(1),'.')
+%                 % if missing dot, assign to object
+%                 self.ext = ['.' self.ext];
+%             end
+%             
+%             % -- disp --
+%             
+%             % stdout display boolean
+%             if disp_chk == 1
+%                 fprintf('Current File: %s \n',self.fullname);
+%             end
+%         end
         
         function fid = open(self,perm)
         %% open() - open file with error screening capability.
@@ -91,16 +109,33 @@ classdef file < matlab.mixin.SetGet
         end  
 
     %% dependent methods
+        function set.name(self,value)
+        %%
+            if isempty(value)
+                error('need a name, dawg');
+            end
+            
+            % look for '.' extension
+            ind = regexp(value,'\.');
+            if length(ind) > 1 % multiple extensions
+                nm = value(1:ind(end)-1); % save name
+                xt = value(ind(end):end);   % save last ext 
+            end
+            
+            self.name = nm;
+            self.ext = xt;
+            
+            
+            
+            
+        end
+        
         function fullname = get.fullname(self)
         %% fullname
         % get full file name based on path, name, and ext.
         % error screen '.txt' 'txt' possibility
             self.chk_name() % chk file name
-            % chk file extension for missing dot
-            if ~strcmp(self.ext(1),'.')
-                % if missing dot, assign to object
-                self.ext = ['.' self.ext];
-            end
+            
             % create full path/file name
             fullname = fullfile(self.path,[self.name self.ext]);
         end
